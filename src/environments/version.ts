d6ecad7d2815e1faa3ee10233cfb4dd08de8b4e2
gnawx <stevanus.satria@gmail.com>
2021-05-09 15:56:03 +0800

    [Sprint 14] take version from npm package.json

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+  dirty: true,
+  raw: '7170d2e-dirty',
+  hash: '7170d2e',
+  distance: null,
+  tag: null,
+  semver: null,
+  suffix: '7170d2e-dirty',
+  semverString: null,
+  version: '0.0.0'
+};
+/* tslint:enable */
